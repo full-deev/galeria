44cc7921a1c34f6cbd3326939e533866b514fb9e
Camilo <74275386+full-deev@users.noreply.github.com>
2024-08-06 17:42:23 -0500

    Add files via upload
    Correcion de errores con try y except, el metodo listbox antes se ajusta depende del tamaño de la ventana ahora es estatico, lo puse dentro de un JFrame, se cambio el nombre del programa a galeria y no en ingles

---
+++ galeria/galeria.py
@@ -0,0 +1,140 @@
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
+
+class Photo:
+    def __init__(self, path):
+        self.path = path
+        try:
+            self.image = Image.open(path)
+            self.thumbnail = self.image.copy()
+            self.thumbnail.thumbnail((150, 150)) # Tamaño de mi imagen
+        except IOError: # Manejo de errores: 
+            messagebox.showerror("Error", f"Revise la ruta: {path}")
+            raise
+
+class Tag: # Puedo realizar un filtro por tag
+    def __init__(self, name):
+        self.name = name
+
+class Album:
+    def __init__(self, name):
+        self.name = name
+        self.photos = []
+        self.tags = []
+
+    def add_photo(self, photo):
+        self.photos.append(photo) 
+
+    def add_tag(self, tag):
+        self.tags.append(tag) # Uso las propiedades de mi lista
+
+class Galeria:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Galeria de fotos By.milo")
+        self.current_album = None
+
+        # Creo opciones tipo Cinta - Ribbon
+        self.ribbon = tk.Frame(root, bg="gray", height=40)
+        self.ribbon.pack(side=tk.TOP, fill=tk.X)
+
+        # Botones dentro de mi cinta ribbon
+        self.add_album_button = tk.Button(
+            self.ribbon, text="Nuevo Album", command=self.add_album
+        )
+        self.add_album_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.load_photo_button = tk.Button(
+            self.ribbon, text="Subir Fotos", command=self.load_photo
+        )
+        self.load_photo_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+        self.exit_button = tk.Button(
+            self.ribbon, text="Salir", command=self.root.quit  # Salir
+        )
+        self.exit_button.pack(side=tk.RIGHT,padx=5, pady=5)
+
+        # Creo un contenedor (frame) para mi listbox - albumes
+        self.listbox_frame = tk.Frame(root, width=200, bg="gray")
+        self.listbox_frame.pack(side=tk.LEFT, fill=tk.Y)
+
+        # Creando listbox_frame
+        self.album_listbox = tk.Listbox(self.listbox_frame, width=25)
+        self.album_listbox.pack(side=tk.LEFT, fill=tk.Y, expand=True)
+
+        # Creo el canvas para mis fotos
+        self.photo_canvas = tk.Canvas(root, bg="gray")
+        self.photo_canvas.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        # Lista de álbumes
+        self.albums = {}
+
+        # Seleccion de un album
+        self.album_listbox.bind('<<ListboxSelect>>', self.load_album)
+
+        # Lista para guardar una imagen luego de subir 
+        self.imagen_refs = []
+
+    def add_album(self):
+        album_name = simpledialog.askstring("Nombre del álbum", "Ingrese el nombre del álbum:")
+        if album_name:
+            album = Album(album_name)
+            self.albums[album_name] = album
+            self.album_listbox.insert(tk.END, album_name)
+
+    def load_album(self, event):
+        selection = self.album_listbox.curselection()
+        if selection:
+            album_name = self.album_listbox.get(selection[0])
+            self.current_album = self.albums[album_name]
+            self.show_photos()
+
+    def load_photo(self):
+        if not self.current_album:
+            messagebox.showwarning("No hay álbum", "Por favor seleccione o agregue un álbum primero")
+            return
+        
+        file_path = filedialog.askopenfilename(
+            filetypes=[("Imagen files", "*.jpg;*.jpeg;*.png")] # Extensiones permitidas
+        )
+        if file_path:
+            photo = Photo(file_path)
+            self.current_album.add_photo(photo)
+            self.show_photos()
+
+    def show_photos(self):
+        if not self.current_album:
+            return
+
+        # Este linea me permite que cada
+        # que creo un nuevo album
+        # no ver la imagen de otro album
+        self.photo_canvas.delete("all") 
+
+        # Dimensiones del cuadro
+        canvas_width = self.photo_canvas.winfo_width()
+        canvas_height = self.photo_canvas.winfo_height()
+
+        # Tamaño de las imagenes y margen
+        thumbnail_size = 150
+        margin = 10
+
+        # Conocer el número de columnas
+        columns = (canvas_width - margin) // (thumbnail_size + margin)
+
+        # Resetear limpiar las imagenes antiguas 
+        self.imagen_refs.clear()
+
+        # Mostrar imagenes en pequeñas
+        for i, photo in enumerate(self.current_album.photos):
+            row = i // columns
+            column = i % columns
+
+            x = margin + column * (thumbnail_size + margin)
+            y = margin + row * (thumbnail_size + margin)
+
+            thumbnail = ImageTk.PhotoImage(photo.thumbnail)
+            self.photo_canvas.create_image(x, y, anchor=tk.NW, image=thumbnail)
+            self.imagen_refs.append(thumbnail) # Guardar mi referencia de la imagen
+            #self.photo_canvas.image = thumbnail
--- galeria/main.py
+++ galeria/main.py
@@ -1,11 +1,14 @@
 # main.py
 import tkinter as tk
-from gallery import Gallery
+from galeria import Galeria
 
 def main():
     root = tk.Tk()
-    gallery = Gallery(root)
+    gallery = Galeria(root)
     root.mainloop()
 
+# Esta condicion me permite ejecutar mi funcion main()
+# siempre y cuando se este ejecutando el archivo main.py
+# con estop
 if __name__ == "__main__":
     main()
